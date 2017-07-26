71866dd3ecdef638e09a1eac9b2da6d3b5bb97d8
marvin <marvinkon@outlook.com>
2017-07-26 14:43:13 -0500

    add bootstraps

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,8 +1,5 @@
 class MenuController < ApplicationController
   def index
-    x = User.all
-    x.each do |y|
-      puts y.email
-    end
+
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
-
+  belongs_to :merchant
+  belongs_to :user
   validates :merchant, presence: {message: "Must select merchant"}
   validates :debit, numericality: { message: "%{value} seems wrong" }, allow_nil: true
   validates :credit, numericality: { message: "%{value} seems wrong" }, allow_nil: true
