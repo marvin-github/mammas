36f8f46ad342a754f4d755cf03867b01d3ee4e42
marvin konwinski <marvinkonwinski@marvins-MacBook-Pro-2.local>
2017-08-05 20:37:11 -0500

    fix invoice edit again +

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,8 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   belongs_to :user
   validates :merchant, presence: {message: "Must select merchant"}
-  validates :debit, numericality: { message: "%{value} seems wrong" }, allow_nil: true
-  validates :credit, numericality: { message: "%{value} seems wrong" }, allow_nil: true
+  validates :extended_cost, numericality: { message: "%{value} seems wrong" }, allow_nil: true
+  validates :price, numericality: { message: "%{value} seems wrong" }, allow_nil: true
   validates :quantity, numericality: { message: "%{value} seems wrong" }, allow_nil: true
 
   def self.to_csv
