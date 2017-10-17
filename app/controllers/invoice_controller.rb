5d6c033d4dd4ee234e6c0e02cf6114175ddd981d
marvin konwinski <marvinkonwinski@marvins-MacBook-Pro-2.local>
2017-10-16 20:36:21 -0500

    change pdf

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -39,18 +39,8 @@ class InvoiceController < ApplicationController
     if params[:selections].blank?
       flash[:danger] = 'Must select at least one item'
       redirect_to :controller => 'invoice', :action => 'new' and return
-    else
-      params[:selections].each do |i|
-        #this is a hack it checks the id of the item to determine if it's for cash accounts only
-        if account_type == 0
-          if i.to_i.between?(22,26)
-            flash[:danger] = 'Items that start with a 6 are for cash accounts only'
-            redirect_to :controller => 'invoice', :action => 'new' and return
-
-          end
-        end
-      end
     end
+
     @items = Item.find(params[:selections])
     @invoice.user_id = session[:user_id]
     puts 'mmmmmmmm'
@@ -72,7 +62,8 @@ class InvoiceController < ApplicationController
           range = item.discount_start_date.strftime("%Y-%m-%d")..item.discount_end_date.strftime("%Y-%m-%d")
           if range.include?(Time.now.strftime("%Y-%m-%d"))
             puts item.discount_amount
-            cart.cost = item.unit_cost * cart.quantity - (item.discount_amount)
+            discounted_item_cost =  item.unit_cost - item.discount_amount
+            cart.cost = discounted_item_cost * cart.quantity
           else
             cart.cost = item.unit_cost * cart.quantity
           end
@@ -94,39 +85,54 @@ class InvoiceController < ApplicationController
   end
 
   def update
-# 'to do: apply discount when updated not sure if it works'
     @invoice = Invoice.find(params[:id])
-
-
     @invoiceItems = Hash.new
-
-    @invoice = Invoice.find(params[:id])
 
     @invoice.invoice_items.each do |ii|
       @invoiceItems[ii.item_id] = ii.id
     end
 
     @invoice.user_id = session[:user_id]
-      flash[:notice] = "Invoice has been updated"
-      params[:selections].each do |i|
-        item = Item.find(i)
-        if @invoiceItems.member?(item.id)
-          unless params['quantity' + i.to_s].blank?
-            x = InvoiceItem.find (@invoiceItems[item.id])
-            x.quantity = params['quantity' + i.to_s]
-            x.cost = item.unit_cost * x.quantity
-            @invoice.invoice_items << x
+    flash[:notice] = "Invoice has been updated"
+    params[:selections].each do |i|
+      item = Item.find(i)
+      if @invoiceItems.member?(item.id)
+        unless params['quantity' + i.to_s].blank?
+          cart = InvoiceItem.find (@invoiceItems[item.id])
+          cart.quantity = params['quantity' + i.to_s]
+          if @invoice.merchant.discount == 'yes' and !item.discount_start_date.nil? and !item.discount_end_date.nil?
+            range = item.discount_start_date.strftime("%Y-%m-%d")..item.discount_end_date.strftime("%Y-%m-%d")
+            if range.include?(Time.now.strftime("%Y-%m-%d"))
+              discounted_item_cost =  item.unit_cost - item.discount_amount
+              cart.cost = discounted_item_cost * cart.quantity
+            else
+              cart.cost = item.unit_cost * cart.quantity
+            end
+          else
+            cart.cost = item.unit_cost * cart.quantity
+          end
+          @invoice.invoice_items << cart
+        end
+      else
+        cart = InvoiceItem.new
+        cart.invoice = @invoice
+        cart.item = item
+        cart.quantity = params['quantity' + i.to_s]
+        if @invoice.merchant.discount == 'yes' and !item.discount_start_date.nil? and !item.discount_end_date.nil?
+          range = item.discount_start_date.strftime("%Y-%m-%d")..item.discount_end_date.strftime("%Y-%m-%d")
+          if range.include?(Time.now.strftime("%Y-%m-%d"))
+            discounted_item_cost =  item.unit_cost - item.discount_amount
+            cart.cost = discounted_item_cost * cart.quantity
+          else
+            cart.cost = item.unit_cost * x.quantity
           end
         else
-          cart = InvoiceItem.new
-          cart.invoice = @invoice
-          cart.item = item
-          cart.quantity = params['quantity' + i.to_s]
           cart.cost = item.unit_cost * cart.quantity
-          @invoice.invoice_items << cart
-
         end
+        @invoice.invoice_items << cart
       end
+    end
+
     if @invoice.update(invoice_params)
       redirect_to @invoice
     else
@@ -143,14 +149,15 @@ class InvoiceController < ApplicationController
     @invoice.invoice_items.each do |ii|
       @invoiceItems[ii.item_id] = ii.id
       @quantities[ii.item_id] = ii.quantity
-   end
-end
+    end
+  end
 
   def destroy
     @invoice = Invoice.find(params[:id])
     @invoice.destroy
     redirect_to invoice_index_path
   end
+
   def delete_item
     print 'lllllllllllll'
     puts params.inspect
--- app/pdfs/invoice_pdf.rb
+++ app/pdfs/invoice_pdf.rb
@@ -3,95 +3,99 @@ class InvoicePdf < Prawn::Document
 
   def initialize(invoice)
     super(top_margin: 70)
+    font_size = 8
     account_type = invoice.merchant.account_type
     invoice.invoice_items.each do |i|
       if  i.item.upc.include? "743010"
-        text "La Mexicana Tortilla Factory", :align => :center
-        text "Wholesale Division", :align => :center
-        text "Billing Remittance:", :align => :center
+        text "La Mexicana Tortilla Factory", :align => :center, :size => 8
+        text "Wholesale Division", :align => :center, :size => 8
+        text "Billing Remittance:", :align => :center, :size => 8
 
-        text "111 E. 4 Street", :align => :center
-        text "Grand Island, NE 68801", :align => :center
-        text "Phone 308-390-0180", :align => :center
+        text "111 E. 4 Street", :align => :center, :size => 8
+        text "Grand Island, NE 68801", :align => :center, :size => 8
+        text "Phone 308-390-0180", :align => :center, :size => 8
         text "_" * 80
-        move_down 30
+        move_down 15
         break
       end
       if account_type == 0 #items that start with a 3 use this heading
-        text "Mi Mama's Tortillas, LLC", :align => :center
-        text "Manufactures of Mi Mama's Tortillas and Baja Wraps", :align => :center
-        text "Billing Remittance:", :align => :center
+        text "Mi Mama's Tortillas, LLC", :align => :center, :size => 8
+        text "Manufactures of Mi Mama's Tortillas and Baja Wraps", :align => :center, :size => 8
+        text "Billing Remittance:", :align => :center, :size => 8
 
-        text "828 S. 17th Street", :align => :center
-        text "Omaha, NE 68108", :align => :center
-        text "Orders: Phone(402) 345-2099 Fax (402) 345-1059", :align => :center
-        text "Billing Questions Phone (402) 345-2099", :align => :center
+        text "828 S. 17th Street", :align => :center, :size => 8
+        text "Omaha, NE 68108", :align => :center, :size => 8
+        text "Orders: Phone(402) 345-2099 Fax (402) 345-1059", :align => :center, :size => 8
+        text "Billing Questions Phone (402) 345-2099", :align => :center, :size => 8
         text "_" * 80
-        move_down 30
+        move_down 15
         break
       end
       if account_type == 1
-        text "Mimick Distributing Incorporated", :align => :center
+        text "Mimick Distributing Incorporated", :align => :center, :size => 8
 
-        text "4001 N 211 Street", :align => :center
-        text "Elkhorn, NE 68022", :align => :center
-        text "(402) 871-7831", :align => :center
-        text "mimickdistributing@hotmail.com", :align => :center
+        text "4001 N 211 Street", :align => :center, :size => 8
+        text "Elkhorn, NE 68022", :align => :center, :size => 8
+        text "(402) 871-7831", :align => :center, :size => 8
+        text "mimickdistributing@hotmail.com", :align => :center, :size => 8
         text "_" * 80
-        move_down 30
+        move_down 15
         break
       end
     end
-    text_box "Invoice Number:", :at => [0, y - 40]
-    text_box  invoice.id.to_s, :at => [130, y - 40]
-    move_down 15
+    text_box "Invoice Number:", :at => [0, y - 40], :size => 8
+    text_box  invoice.id.to_s, :at => [130, y - 40], :size => 8
+    move_down 10
 
-    text_box "Customer Account No:", :at => [0, y - 40]
-    text_box  invoice.merchant.store_number,:at => [130, y - 40]
-    move_down 15
+    text_box "Customer Account No:", :at => [0, y - 40], :size => 8
+    text_box  invoice.merchant.store_number,:at => [130, y - 40], :size => 8
+    move_down 10
 
-    text_box "Name:", :at => [0, y - 40]
-    text_box invoice.merchant.merchant_name,:at => [130, y - 40]
-    move_down 15
+    text_box "Name:", :at => [0, y - 40], :size => 8
+    text_box invoice.merchant.merchant_name,:at => [130, y - 40], :size => 8
+    move_down 10
 
-    text_box "Billing Address:",:at => [0, y - 40]
-    text_box invoice.merchant.address1, :at => [130, y - 40]
-    move_down 15
+    text_box "Billing Address:",:at => [0, y - 40], :size => 8
+    text_box invoice.merchant.address1, :at => [130, y - 40], :size => 8
+    move_down 10
 
-    text_box "City:",:at => [0, y - 40]
-    text_box invoice.merchant.city, :at => [130, y - 40]
-    move_down 15
+    text_box "City:",:at => [0, y - 40], :size => 8
+    text_box invoice.merchant.city, :at => [130, y - 40], :size => 8
+    move_down 10
 
-    text_box "Phone:",:at => [0, y - 40]
-    text_box invoice.merchant.phone, :at => [130, y - 40]
-    move_down 15
+    text_box "Phone:",:at => [0, y - 40], :size => 8
+    text_box invoice.merchant.phone, :at => [130, y - 40], :size => 8
+    move_down 10
 
-    text_box "Date:",:at => [0, y - 40]
-    text_box invoice.start_date.strftime("%m-%d-%Y"), :at => [130, y - 40]
+    text_box "Date:",:at => [0, y - 40], :size => 8
+    text_box invoice.start_date.strftime("%m-%d-%Y"), :at => [130, y - 40], :size => 8
 
-    move_down 40
-    text 'Detail', :align => :center, :style => :bold
     move_down 30
+    text 'Detail', :align => :center, :style => :bold, :size => 8
+    move_down 10
     cash = 0.0
-    credit = 0.0
     total = 0.0
+    credit = 0.0
+
 
     #text 'UPC' + '  |  ' +   'Description' + '  |  '  + 'Quantity' + '  |  ' + 'Cost', :style => :bold
-    text_box "UPC",      :at => [0, y - 40]
-    text_box "Description",   :at => [100, y - 40]
-    text_box "Quantity",   :at => [275, y - 40]
-    text_box "Cost",   :at => [350, y - 40]
-    text_box "Extended Price",   :at => [425, y - 40]
+    text_box "UPC",      :at => [0, y - 40], :size => 8
+    text_box "Description",   :at => [65, y - 40], :size => 8
+    text_box "Package Counts",   :at => [200, y - 40], :size => 8
+    text_box "Quantity",   :at => [300, y - 40], :size => 8
+    text_box "Cost",   :at => [350, y - 40], :size => 8
+    text_box "Extended Price",   :at => [400, y - 40], :size => 8
     move_down 25
 
     invoice.invoice_items.each do |i|
       #text i.item.upc + '  |  ' +   i.item.description  + '  |  '  + i.quantity.to_s + '  |  ' + sprintf('%.2f',i.item.unit_cost).to_s
-      text_box i.item.upc,      :at => [0, y - 40]
-      text_box i.item.description,   :at => [100, y - 40]
-      text_box i.quantity.to_s,   :at => [275, y - 40]
-      text_box "$"+ sprintf('%.2f',i.item.unit_cost).to_s,   :at => [350, y - 40]
-      text_box "$"+ sprintf('%.2f',i.item.unit_cost * i.quantity).to_s,   :at => [425, y - 40]
-      move_down 20
+      text_box i.item.upc,      :at => [0, y - 40], :size => 8
+      text_box i.item.description,   :at => [65, y - 40], :size => 8
+      text_box i.item.package_counts,   :at => [200, y - 40], :size => 8
+      text_box i.quantity.to_s,   :at => [300, y - 40], :size => 8
+      text_box "$"+ sprintf('%.2f',i.item.unit_cost).to_s,   :at => [350, y - 40], :size => 8
+      text_box "$"+ sprintf('%.2f',i.item.unit_cost * i.quantity).to_s,   :at => [400, y - 40], :size => 8
+      move_down 10
 
       total += i.item.unit_cost * i.quantity
       if i.quantity < 0
@@ -102,14 +106,12 @@ class InvoicePdf < Prawn::Document
     end
     move_down 30
 
-    draw_text "Cash Total    " + "$" + sprintf('%.2f',cash).to_s,:at => [350, y - 40]
-    move_down 14
-    draw_text "Credit Total   " + "$" + sprintf('%.2f',credit).to_s,:at => [350, y - 40]
-    move_down 14
-    draw_text "Grand Total   " + "$" + sprintf('%.2f',total).to_s,:at => [350, y - 40]
-    move_down 200
+    draw_text "Total",:at => [350, y - 40], :size => 8
+    draw_text "$" + sprintf('%.2f',cash).to_s,:at => [400, y - 40], :size => 8
+    move_down 30
 
-    text "Received By _________________________________________________________"
+
+    text "Received By _______________________________________", :size => 8
     #stroke_axis
 
 
